9bb3afdc0146125e9e6ac0b5361dc62fa8614ad1
gaughanln <gaughanln@yahoo.com>
2023-01-27 11:20:07 -0600

    working through license functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+// const { create } = require('domain');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [
+//   'Enter the description.',
+//   'What are the installation instructions:',
+//   'Enter usage information for the application',
+//   'Who contributed to this project?',
+//   'What are the test instructions',
+// ];
+
+inquirer
+.prompt ([ 
+//   {
+//   type: 'input',
+//   name: 'title',
+//   message: 'What is the title of your README?',
+// },
+{
+  type: 'input',
+  name: 'description',
+  message: 'Enter the description of the application.',
+},
+{
+  type: 'input',
+  name: 'installation',
+  message: 'What are the installation instructions?',
+},
+{
+  type: 'input',
+  name: 'usage',
+  message: 'Enter usage information for the application',
+},
+{
+  type: 'input',
+  name: 'contribution',
+  message: 'Who contributed to this project?',
+},
+{
+  type: 'input',
+  name: 'test',
+  message: 'What are the test instructions?',
+},
+])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+.then((content) => {
+
+readmeContent = generateMarkdown(content)
+
+fs.writeToFile(README2.md, readmeContent, (err) => {
+  if (err)
+  console.log(err)
+  else {
+    console.log("Files written successfully")
+  }
+})
+})
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
+ 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no license, return an empty string - NEED A SWITCH CASE FOR THIS. 
+
+// this initially read function renderLicenseBadge(license){} - but I changed it to an arrow function for practice. is that ok?
+renderLicenseBadge = (license) => {
+  switch(license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+      case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+      case 'GNU General Public 3.0':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+      default:
+       return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// I INCLUDED THIS IN THE FUNCTION ABOVE - IS THIS OK? DO I NEED 2 FUNCTIONS?
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      return '(https://opensource.org/licenses/MIT)';
+      break;
+      case 'Apache 2.0':
+      return '(https://opensource.org/licenses/Apache-2.0)';
+      break;
+      case 'GNU General Public 3.0':
+      return '(https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+      default:
+       return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// template literal for readme 
+const generateMarkdown = ({data}) => `
+# ${data.title} 
+## Description 
+${data.description} 
+### Installation 
+${data.installation}
+### Usage 
+${data.usage}
+### Contributing 
+${data.contributing}
+### Tests 
+${data.tests}`
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
 module.exports = generateMarkdown;
